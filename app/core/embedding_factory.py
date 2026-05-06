b718285197043ad85c92d1eff592dc653395448f
day811 <day811@laposte.net>
2026-05-07 01:19:10 +0200

    feat: add mssing params to azure embeddings ref #14

--- app/core/embedding_factory.py
+++ app/core/embedding_factory.py
@@ -10,10 +10,19 @@ class EmbeddingFactory:
         env = os.getenv("ENV", "LOCAL").upper()
         
         if env == "AZURE":
+            # On récupère proprement les variables
+            endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+            api_key = os.getenv("AZURE_OPENAI_API_KEY","")
+            deployment = os.getenv("AZURE_EMBEDDING_DEPLOYMENT")
+            # On force une version récente si la variable est vide
+            api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2024-02-15-preview")
+
             return AzureOpenAIEmbeddings(
-                azure_deployment=os.getenv("AZURE_EMBEDDING_DEPLOYMENT"),
-                api_key=os.getenv("AZURE_OPENAI_API_KEY")
-            )
+                azure_endpoint=endpoint,
+                azure_deployment=deployment,
+                api_key=api_key,
+                api_version=api_version
+        )
         else:
             return MistralAIEmbeddings(
                 model="mistral-embed",
